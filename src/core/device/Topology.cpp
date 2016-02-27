852db17e16d8944f3edb1e0c8633b6447cc0c473
Kaushik Kanetkar <kaushik.ajit.kanetkar@intel.com>
2016-02-27 06:42:45 +0100

    Implement topology classes in core
    - Implement core::device::Topology
    
    Signed-off-by: Kaushik Kanetkar <kaushik.ajit.kanetkar@intel.com>

---
+++ src/core/device/Topology.cpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2016, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Intel Corporation nor the names of its contributors
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <LogEnterExit.h>
+#include "Topology.h"
+
+const memory_topology &core::device::Topology::getTopology()
+{
+	LogEnterExit logging(__FUNCTION__, __FILE__, __LINE__);
+	return m_topology;
+}
+NVM_UINT16 core::device::Topology::getPhysicalID()
+{
+	LogEnterExit logging(__FUNCTION__, __FILE__, __LINE__);
+	return getTopology().physical_id;
+}
+enum memory_type core::device::Topology::getMemoryType()
+{
+	LogEnterExit logging(__FUNCTION__, __FILE__, __LINE__);
+	return getTopology().memory_type;
+}
+enum device_form_factor core::device::Topology::getFormFactor()
+{
+	LogEnterExit logging(__FUNCTION__, __FILE__, __LINE__);
+	return getTopology().form_factor;
+}
+NVM_UINT64 core::device::Topology::getRawCapacity()
+{
+	LogEnterExit logging(__FUNCTION__, __FILE__, __LINE__);
+	return getTopology().raw_capacity;
+}
+NVM_UINT64 core::device::Topology::getDataWidth()
+{
+	LogEnterExit logging(__FUNCTION__, __FILE__, __LINE__);
+	return getTopology().data_width;
+}
+NVM_UINT64 core::device::Topology::getTotalWidth()
+{
+	LogEnterExit logging(__FUNCTION__, __FILE__, __LINE__);
+	return getTopology().total_width;
+}
+NVM_UINT64 core::device::Topology::getSpeed()
+{
+	LogEnterExit logging(__FUNCTION__, __FILE__, __LINE__);
+	return getTopology().speed;
+}
+std::string core::device::Topology::getPartNumber()
+{
+	LogEnterExit logging(__FUNCTION__, __FILE__, __LINE__);
+	return getTopology().part_number;
+}
+std::string core::device::Topology::getDeviceLocator()
+{
+	LogEnterExit logging(__FUNCTION__, __FILE__, __LINE__);
+	return getTopology().device_locator;
+}
+std::string core::device::Topology::getBankLabel()
+{
+	LogEnterExit logging(__FUNCTION__, __FILE__, __LINE__);
+	return getTopology().bank_label;
+}
+core::device::Topology *core::device::Topology::clone()
+{
+	LogEnterExit logging(__FUNCTION__, __FILE__, __LINE__);
+	return new Topology(*this);
+}
---
+++ src/core/device/Topology.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2016, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Intel Corporation nor the names of its contributors
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef CR_MGMT_TOPOLOGY_H
+#define CR_MGMT_TOPOLOGY_H
+
+
+#include <string>
+#include <nvm_management.h>
+#include <core/Helper.h>
+#include <common_types.h>
+#include <system/jedec_manufacturer.h>
+#include <utility.h>
+#include <nvm_types.h>
+
+#include <string>
+#include <vector>
+#include <string/s_str.h>
+#include <sstream>
+#include <core/exceptions/LibraryException.h>
+#include <core/NvmApi.h>
+#include <core/Collection.h>
+#include "Device.h"
+
+namespace core
+{
+namespace device
+{
+class NVM_API Topology
+{
+public:
+	Topology(const memory_topology &topology)
+	{
+		memmove(&m_topology, &topology, sizeof(m_topology));
+	}
+
+	Topology(const Topology &other)
+	{
+		this->m_topology = other.m_topology;
+	}
+
+	Topology &operator=(const Topology &other)
+	{
+		if (&other == this)
+			return *this;
+		this->m_topology = other.m_topology;
+		return *this;
+	}
+
+	Topology *clone();
+	NVM_UINT16 getPhysicalID();
+	enum memory_type getMemoryType();
+	enum device_form_factor getFormFactor();
+	NVM_UINT64 getRawCapacity();
+	NVM_UINT64 getDataWidth();
+	NVM_UINT64 getTotalWidth();
+	NVM_UINT64 getSpeed();
+	std::string getPartNumber();
+	std::string getDeviceLocator();
+	std::string getBankLabel();
+
+private:
+	const memory_topology &getTopology();
+	memory_topology m_topology;
+};
+
+class NVM_API TopologyCollection : public Collection<Topology>
+{
+};
+}
+}
+#endif //CR_MGMT_TOPOLOGY_H
